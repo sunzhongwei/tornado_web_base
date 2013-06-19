6937be3dbeb37546fd7df9edffbe56b142a0884a
Zhongwei Sun <zhongwei.sun2008@gmail.com>
2013-06-19 20:53:36 +0800

    update

--- bin/init_tornado_web_base.sh
+++ bin/init_tornado_web_base.sh
@@ -10,7 +10,7 @@ set -x  # or use "set -o xtrace" to print the statement before you execute it.
 CUR_FILE_DIR=$(cd "$(dirname "$0")"; pwd)
 RUNNING_DIR=$(pwd)
 cp -R $CUR_FILE_DIR/../src $RUNNING_DIR/src
-ln -nfs /Users/zhongwei/life/tornado/tornado $RUNNING_DIR/src/tornado
+#ln -nfs /Users/zhongwei/life/tornado/tornado $RUNNING_DIR/src/tornado
 
 mkdir -p $RUNNING_DIR/bin
 cp $CUR_FILE_DIR/new_handler.py $RUNNING_DIR/bin/new_handler.py
